825adf38aef1062873eeaf856b8d3c14da4d0ca8
Irene Lam <ilam1@stuy.edu>
2017-12-12 15:44:58 -0500

    Color changes with mouse movement

--- find.js
+++ find.js
@@ -11,16 +11,26 @@ var targetY = boxHeight / 2;
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
 
-//calculate distance between current mouse pos and target
+//calculate distance between current mouse pos and target (returns an integer)
 var distance = function (x0, y0, x1, y1) {
-  return sqrt(Math.pow(y1-y0)+Math.pow(x1-x0));
+    return Math.round(Math.hypot(x1-x0, y1-y0));
+  //return Math.sqrt(Math.pow(y1-y0)+Math.pow(x1-x0));
 };
 
-
 var findIt = function(e) {
-    var body = document.getElementsByClassName("full");
-    console.log(e.X);
-    console.logg(e.Y);
+    //var body = document.getElementsByClassName("full");
+
+    //console.log(color);
+    var x = event.clientX;
+    var y = event.clientY;
+    //console.log(x);
+    var away = distance(targetX, targetY, x, y);
+    console.log(away);
+    var body = document.getElementById("box");
+    var bcolor = body.getAttribute("style");
+    body.setAttribute("style", "background-color: rgb(0, 0, " + (256-away) + ");");
+    //document.getElementById("box").style.background-color.innerHTML = "rgb(0,0,"+away+")";
+    //body.innerHTML("background-color")
 };
 
 /*
