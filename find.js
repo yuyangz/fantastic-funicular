2c659ad866b97fa6b7b1436db92a36341379c28c
Yuyang Zhang <yzhang4@stuy.edu>
2017-12-13 10:53:53 -0500

    Updated score system

--- find.js
+++ find.js
@@ -10,7 +10,7 @@ var box = document.getElementById("box");
 var boxHeight = box.offsetHeight;
 var boxWidth = box.offsetWidth;
 
-<<<<<<< HEAD
+
 var targetX = boxWidth/2;
 var targetY = boxHeight/2;
 /*
@@ -18,13 +18,12 @@ var targetX = boxWidth * Math.random();
 var targetY = boxHeight * Math.random();
 */
 
-=======
 //hardcode target as center
 //randomize later
 var targetX = Math.random() * boxWidth;//boxWidth / 2;
 var targetY = Math.random() * boxHeight;//boxHeight / 2;
 var away = 30;
->>>>>>> 5aa765630c4858e0a380e132d542fe8f771ffc38
+
 
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
@@ -46,11 +45,11 @@ var findIt = function(e) {
     //goal(away));
 };
 
-<<<<<<< HEAD
+
 var score = 0;
 
 var addScore = function(e){
-    if (distance <= 10) {
+    if (away < 20) {
 	score += 1;
 	console.log("Your score is now " + score);
     }
@@ -59,21 +58,21 @@ var addScore = function(e){
     }
 }
 
-=======
->>>>>>> 5aa765630c4858e0a380e132d542fe8f771ffc38
+
 
 //Helper method that gives a congratulations when the mouse is close enough to the goal
 var goal = function(e) {
     if (away < 20) {
         console.log("Congratulations!");
         var body = document.getElementsByClassName("full");
-        body[0].innerHTML = "Congratulations! You've successfully found the (hidden) cow!";
+	targetX = Math.random() * boxWidth;
+	targetY = Math.random() * boxHeight;
+       
     }
 }
 box.addEventListener("mousemove", findIt);
-<<<<<<< HEAD
+
 box.addEventListener("click", addScore);
-=======
+
 box.addEventListener("click", goal);
->>>>>>> 5aa765630c4858e0a380e132d542fe8f771ffc38
 
